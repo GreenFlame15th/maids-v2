d98db9c7d813e759f3b3b4b4533610fe6e1cf968
GreenFlame15th <GreenFlame133@gmail.comm>
2021-05-24 14:16:08 +0200

    coment added

--- run.js
+++ run.js
@@ -7,3 +7,12 @@ console.log('Waking up the maids')
 
 new bot('./green', tokenGreen)
 new bot('./blue', tokenBlue)
+
+
+//keep the bot doing something to prevent time out
+async function boop() {
+  setTimeout(() => {
+    boop();
+    console.log("boop");
+  }, 60 * 1000);
+}
