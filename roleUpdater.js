ec784a64165a8b178114bb643c3d157f1a1b410e
GreenFlame15th <GreenFlame133@gmail.comm>
2021-05-26 08:25:23 +0200

    Made roleUpdater asynchronous

--- roleUpdater.js
+++ roleUpdater.js
@@ -1,11 +1,16 @@
-function login(user, fusionRoles, roles, log) {
+async function login(user, fusionRoles, roles, log) {
 
-    if(user.user.bot) {return}
-    if(!user.roles.cache.map(r=>r.id).includes(roles.verify)){return;}
+    if (user.user.bot) {
+        return
+    }
+    else if (!user.roles.cache.map(r => r.id).includes(roles.verify)) {
+        return;
+    }
+    else {
 
-    const userRoles = user.roles.cache
-    const userRoleIds = userRoles.array().map(r => r.id)
-    const guildRoles = user.guild.roles.cache
+    const userRoles = await user.roles.cache
+    const userRoleIds = await userRoles.array().map(r => r.id)
+    const guildRoles = await user.guild.roles.cache
 
 
     let nsfw = false
@@ -15,34 +20,43 @@ function login(user, fusionRoles, roles, log) {
                 nsfw = true
             }
         })
-    function addById(id){
-        if(!userRoleIds.includes(id) && id.length === 18)
+
+    function addById(id) {
+        if (!userRoleIds.includes(id) && id.length === 18)
             user.roles.add(guildRoles.get(id))
-                .then(log.send("Added **"+guildRoles.get(id).name+"** to <@" +user.id+ ">"))
-                .catch(err => console.log("RoleUpdater, adding "+id+" to "+user.nickname+": " + err))
+                .then(log.send("Added **" + guildRoles.get(id).name + "** to <@" + user.id + ">"))
+                .catch(err => console.log("RoleUpdater, adding " + id + " to " + user.nickname + ": " + err))
     }
-    function removeById(id){
-        if(userRoleIds.includes(id) && id.length === 18)
+
+    function removeById(id) {
+        if (userRoleIds.includes(id) && id.length === 18)
             user.roles.remove(guildRoles.get(id))
-                .then(log.send("Removed **"+guildRoles.get(id).name+"** from <@" +user.id+ ">"))
-                .catch(err => console.log("RoleUpdater, removing "+id+" from "+user.nickname+": " + err))
+                .then(log.send("Removed **" + guildRoles.get(id).name + "** from <@" + user.id + ">"))
+                .catch(err => console.log("RoleUpdater, removing " + id + " from " + user.nickname + ": " + err))
     }
+
     //nsfw fusion
-    if(nsfw){
-        fusionRoles.nsfwFusion.forEach(fu =>{
-            if (userRoleIds.includes(fu[0])) {addById(fu[1])}
-            else
-                if (!userRoleIds.includes(fu[0])) {removeById(fu[1])}
-            })
-    }else
+    if (nsfw) {
         fusionRoles.nsfwFusion.forEach(fu => {
-            if (userRoleIds.includes(fu[1])) {removeById(fu[1])}
+            if (userRoleIds.includes(fu[0])) {
+                addById(fu[1])
+            } else if (!userRoleIds.includes(fu[0])) {
+                removeById(fu[1])
+            }
+        })
+    } else
+        fusionRoles.nsfwFusion.forEach(fu => {
+            if (userRoleIds.includes(fu[1])) {
+                removeById(fu[1])
+            }
         })
     //vvv here goes date/fusion role handler vvv
-    fusionRoles.fusion.forEach( f=>{
-        if(f[1].every(r=>userRoleIds.includes(r))){addById(f[0])}
-        else {removeById(f[0])}
+    fusionRoles.fusion.forEach(f => {
+        if (f[1].every(r => userRoleIds.includes(r))) {
+            addById(f[0])
+        } else {
+            removeById(f[0])
+        }
     })
-}
-
+}}
 module.exports = login;
